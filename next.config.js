81e92d6394e85fb7ea3f8f105074bc883e207745
Manish <saini2000manish@gmail.com>
2021-09-20 17:21:27 +0530

    First commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+        domains: ["links.papareact.com"],
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1Ijoic2FpbmltYW5pc2giLCJhIjoiY2tva2Q3N2lvMGRxYjJ3cnZ4dDk2YnhpMyJ9.pkhxszMAtiFmMLIMyK78Dg'
+    }
+};
